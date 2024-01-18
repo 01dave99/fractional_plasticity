9736b2a6c2a9cae2361eccda21d39d61311e96a9
David <niederkofler.david@hotmail.de>
2024-01-18 20:30:57 +0100

    m

--- mises_2d_hard_simulation.py
+++ mises_2d_hard_simulation.py
@@ -52,7 +52,7 @@ def eval_S(sigma_tr,chi1,gamma):
 
 
 #Finite Element specifications:
-for mshi in range(9,11):
+for mshi in range(11,12):
 	#mesh
 
 	with io.XDMFFile(MPI.COMM_WORLD, "meshes/mesh2d_"+str(mshi)+".xdmf", "r") as xdmf:
